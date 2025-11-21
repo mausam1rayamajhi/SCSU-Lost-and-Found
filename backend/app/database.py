ff89d7470d575ab216d6807c8b8e2ef3c5428fa4
Mausam Rayamajhi <rayamajhimausam@gmail.com>
2025-11-21 16:27:13 -0600

    updating database for neon

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,15 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from .config import settings
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URL, echo=False, future=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
-
-def get_db():
-    from sqlalchemy.orm import Session
-    db: Session = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
